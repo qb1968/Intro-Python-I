557732f2e945b2dd54057f2e46257440731fe55b
qb1968 <mark.allison75@gmail.com>
2020-05-04 18:24:55 -0400

    Finished

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,7 +1,10 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
-
+def is_even(x):
+    if (x % 2) == 0:
+        return True
+    return False
 # Read a number from the keyboard
 num = input("Enter a number: ")
 num = int(num)
@@ -9,4 +12,9 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
-
+def is_even2(x):
+    if (x % 2) == 0:
+        print("Even!")
+    else:
+        print("Odd")
+print(is_even2(num))
--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,10 +10,18 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
-
+with open("src/foo.txt") as file:
+    print(file.read())
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+myPoem = """
+She's my other half, my sidekick on the side.
+She makes me, me, we are partners in crime
+Where she is, I am not far behind.
+"""
+with open("bar.txt", "w") as file:
+    file.write(myPoem) 
